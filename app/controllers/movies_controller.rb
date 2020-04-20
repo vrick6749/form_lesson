a1bce2140aa2216c2172527afec2cda9fe776031
vrick6749 <vrick6749@gmail.com>
2020-04-20 22:23:09 +1000

    added views

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,14 +1,21 @@
 class MoviesController < ApplicationController
     def index
+        @movies = session[:movies]
     end
 
     def create
-        
+        if session[:movies] == nil
+            session[:movies]=[]
+        end
+        session[:movies].push(params[:movie])
+
+        redirect_to movie_path(session[:movies].length -1)
     end
 
     def new
     end
 
     def show
+        @movie = session[:movies][params[:id].to_i -1]
     end
 end
