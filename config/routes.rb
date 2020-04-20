e5bed406274f43068b01202506d1e76a4f633131
vrick6749 <vrick6749@gmail.com>
2020-04-20 18:04:39 +1000

    added routes with shortcut as

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  GET "/movies", to: "movies#index", as: "movies"  
+  POST "/movies", to: "movies#create"
+  GET "/movies/new", to: "movies#new", as: "new_movie"
+  GET "/movies/:id", to: "movies#show", as: "movie"
 end
